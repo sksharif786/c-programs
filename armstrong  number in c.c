32c0de364091823e32a3e01d2a5108d66bdc9db7
SHAIK SHARIF <117517970+sksharif786@users.noreply.github.com>
2023-06-04 11:43:38 +0530

    Add files via upload

---
+++ armstrong  number in c.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int number=143,remainder,temp=0,sum=0,c;
+    temp=number;
+    while(number!=0)
+    {
+        remainder=number%10;
+        c=remainder*remainder*remainder;
+        sum=sum+c;
+        number=number/10;   
+    }
+    if(temp==sum)
+    {
+        printf("it is a arm strong number");
+    }
+    else{
+        printf("it is not a arm strong number");
+    }
+}
