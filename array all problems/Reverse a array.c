3ce159b83eb5c245321de0ffc232be06b28dc5ef
SHAIK SHARIF <117517970+sksharif786@users.noreply.github.com>
2023-06-05 21:22:07 +0530

    Create Reverse a array.c

---
+++ array all problems/Reverse a array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int size;
+  printf("enter the size");
+  scanf("%d",&size);
+  int arr[size];
+  printf("enter the elements");
+  int i;
+  for(int i=0;i<size;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+  for(int i=size-1;i>=0;--i)
+  {
+    printf("%d ",arr[i]);
+  }
+}
