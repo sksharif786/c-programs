6bf7e12ed8d39d6296284023f2c5a057e38719dc
SHAIK SHARIF <117517970+sksharif786@users.noreply.github.com>
2023-06-06 21:05:43 +0530

    Update sum of diagonals.c

--- array all problems/MATRIX PROGRAMS/sum of diagonals.c
+++ array all problems/MATRIX PROGRAMS/sum of diagonals.c
@@ -1,1 +1,42 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  
+int size_row,size_col,sum=0;
+printf("enter the row and col size:");
+scanf("%d %d",&size_row,&size_col);
+int arr[size_row][size_col];
+for (int i=0;i<size_row;i++)
+{
+  for(int j=0;j<size_col;j++)
+  {
+    scanf("%d",&arr[i][j]);
+  }
+}
+for (int i=0;i<size_row;i++)
+{
+  for(int j=0;j<size_col;j++)
+  {
+    printf("%d\t",arr[i][j]);
+  }
+  printf("\n");
+}
+printf("the diagonals");
+for (int i=0;i<size_row;i++)
+{
+  for(int j=0;j<size_col;j++)
+  {
+    if(arr[i]==arr[j])
+    {
+    printf("%d\t",arr[i][j]);
+    sum=sum+arr[i][j];
+    }
+  }
+  printf("\n");
+}
+printf("the sum of diagonals are:%d",sum);
+
+
+}
