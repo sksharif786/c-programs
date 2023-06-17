e6c3ae060fb4f02fd23e14264a02a3d444303fc2
SHAIK SHARIF <117517970+sksharif786@users.noreply.github.com>
2023-06-17 14:55:57 +0530

    Update Write a program in C to input a string and print it.c

--- string all problems/Write a program in C to input a string and print it.c
+++ string all problems/Write a program in C to input a string and print it.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int  main()
+{
+  //char str[]  (or)  char *str both are same
+  char *str;
+  printf("enter the string:");
+  //  %[^\n]s  is used to get full string
+  scanf("%[^\n]s",str);
+  printf("%s",str);
+}
